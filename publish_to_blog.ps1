cda751f57646630e0dae30628f50d36ba0eb1951
Gary Kenneally <garykenneally@gmail.com>
2014-10-19 19:06:51 +0100

    publish to blog

--- publish_to_blog.ps1
+++ publish_to_blog.ps1
@@ -1,6 +1,7 @@
+rmdir ./public
+
 # Add the blog folder repo as a subtree under the 'public' folder.
 git subtree add --prefix public git@github.com:SecretDeveloper/secretdeveloper.github.io.git hugo --squash
-
 
 # pull down the blog repo into folder.
 git subtree pull --prefix=public
