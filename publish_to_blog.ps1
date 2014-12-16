660ee2dbdafc56e0e6f561d84f01ce848e9ace49
Gary Kenneally <garykenneally@gmail.com>
2014-12-16 22:33:26 +0000

    commit

--- publish_to_blog.ps1
+++ publish_to_blog.ps1
@@ -3,24 +3,16 @@ rmdir ./public -r -force
 
 mkdir ./public
 
-write-host 'git subtree blog into /public'
-# Add the blog folder repo as a subtree under the 'public' folder.
-git subtree add --prefix public git@github.com:SecretDeveloper/secretdeveloper.github.io.git hugo --squash
-
-write-host 'git pull blog repo into /public'
-# pull down the blog repo into folder.
-git subtree pull --prefix=public git@github.com:SecretDeveloper/secretdeveloper.github.io.git hugo
-
 write-host 'hugo build into /public'
 # publish to 'publish'
 ./hugo.exe -t purehugo
 
-write-host 'git add, commit, push repo'
-# push to repo
-git add --all .
-git commit -m "publish to blog" 
-git push origin master
+mv ./public/* ../secretdeveloper.github.io/
 
-write-host 'git push /public to blog'
-# push to blog repo from /publish
-git subtree push --prefix public git@github.com:SecretDeveloper/secretdeveloper.github.io.git hugo --squash
+cd ../secretdeveloper.github.io/
+
+git add --all .
+git commit -am "automated post"
+git push -u origin master
+
+cd ../hugo-site
