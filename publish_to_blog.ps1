a9f872e2b7226490db15155cfb0bbb949322cbfd
Gary Kenneally <GaryKenneally@gmail.com>
2018-09-21 12:38:11 +0100

    hugo version update, theme change and a bunch of edits to correct minor issues (links, image URLs)

--- publish_to_blog.ps1
+++ publish_to_blog.ps1
@@ -5,7 +5,18 @@ mkdir ./public
 
 write-host 'hugo build into /public'
 # publish to 'publish'
-./hugo.exe -t purehugo
+./hugo.exe -t tale
+
+#clear the blog folder content.
+rmdir ..\secretdeveloper.github.io\about -r -force
+rmdir ..\secretdeveloper.github.io\categories -r -force
+rmdir ..\secretdeveloper.github.io\css -r -force
+rmdir ..\secretdeveloper.github.io\images -r -force
+rmdir ..\secretdeveloper.github.io\page -r -force
+rmdir ..\secretdeveloper.github.io\post -r -force
+rmdir ..\secretdeveloper.github.io\presentations -r -force
+rm ..\secretdeveloper.github.io\*.xml
+rm ..\secretdeveloper.github.io\*.html
 
 cp .\public\* ..\secretdeveloper.github.io -force -recurse
 
